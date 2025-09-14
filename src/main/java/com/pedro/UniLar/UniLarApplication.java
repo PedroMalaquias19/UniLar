18c9aae55dc235a4294608901c3a45cf84c82728
Goel Nkoko <nkembwankumbo@gmail.com>
2025-09-14 09:01:14 +0100

    Initial Commit

---
+++ src/main/java/com/pedro/UniLar/UniLarApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.UniLar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniLarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniLarApplication.class, args);
+	}
+
+}
